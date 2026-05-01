fce88e4304ddc6635f82c0f489344d074af5be6e
rdsymlntsa <rdsymlntsa@gmail.com>
2026-05-01 14:37:17 +0530

    feat: Add redis

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import { createClient } from "redis";
 
 dotenv.config();
 
 connectDb();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL!,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("Connected to redis"))
+  .catch(console.error);
 
 const app = express();
 
