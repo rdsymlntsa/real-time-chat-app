f26ced8e8d64411c3bc6a273302f4ca8ca3869c0
rdsymlntsa <rdsymlntsa@gmail.com>
2026-05-02 12:15:20 +0530

    feat: Add redis client connection. feat: Setup rabbitmq to add task to the queue for otp. feat: Add consumer.ts that sends otp from the queue. feat: Add service for mail

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import { createClient } from "redis";
+import userRoutes from "./routes/user.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
 
 dotenv.config();
 
 connectDb();
+
+connectRabbitMQ();
 
 export const redisClient = createClient({
   url: process.env.REDIS_URL!,
@@ -17,6 +21,8 @@ redisClient
   .catch(console.error);
 
 const app = express();
+
+app.use("api/v1", userRoutes);
 
 const port = process.env.PORT;
 
