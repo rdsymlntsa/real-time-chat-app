903d16de67b9ec239aebd190fceea045387993b6
rdsymlntsa <rdsymlntsa@gmail.com>
2026-05-15 00:00:45 +0530

    feat: Add functionalities to logout , fetch users and fetch chats to app context. feat: Add chats , users , set chats to app context

--- frontend/src/components/VerifyOtp.tsx
+++ frontend/src/components/VerifyOtp.tsx
@@ -9,7 +9,14 @@ import Loading from "./Loading";
 import toast from "react-hot-toast";
 
 const VerifyOtp = () => {
-  const { isAuth, setIsAuth, setUser, loading: userLoading } = useAppData();
+  const {
+    isAuth,
+    setIsAuth,
+    setUser,
+    loading: userLoading,
+    fetchUsers,
+    fetchChats,
+  } = useAppData();
   const [loading, setLoading] = useState(false);
   const [otp, setOtp] = useState<string[]>(["", "", "", "", "", ""]);
   const [error, setError] = useState<string>("");
@@ -88,6 +95,8 @@ const VerifyOtp = () => {
       inputRefs.current[0]?.focus();
       setUser(data.user);
       setIsAuth(true);
+      fetchUsers();
+      fetchChats();
     } catch (error: any) {
       setError(error.response.data.message);
     } finally {
--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -44,6 +44,12 @@ interface AppContextType {
   isAuth: boolean;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
   setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+  logoutUser: () => Promise<void>;
+  fetchUsers: () => Promise<void>;
+  fetchChats: () => Promise<void>;
+  chats: Chats[] | null;
+  users: User[] | null;
+  setChats: React.Dispatch<React.SetStateAction<Chats[] | null>>;
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -56,17 +62,14 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
   const [isAuth, setIsAuth] = useState(false);
   const [loading, setLoading] = useState(true);
-
   async function fetchUser() {
     try {
       const token = Cookies.get("token");
-
       const { data } = await axios.get(`${user_service}/api/v1/me`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-
       setUser(data);
       setIsAuth(true);
       setLoading(false);
@@ -75,38 +78,61 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
       setLoading(false);
     }
   }
-
-  async function logoutUser(){
+  async function logoutUser() {
     Cookies.remove("token");
     setUser(null);
     setIsAuth(false);
     toast.success("User Logged Out");
   }
-
-  const [chats,setChats]=useState<Chats[] | null>(null);
-
-  async function fetchChats(){
-    const token=Cookies.get("token");
-    try{
-  const {data}=await axios.get(`${chat_service}/api/v1/chat/all`,{
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-  setChats(data.chats);
-    }
-    catch(error){
-console.log(error);
+  const [chats, setChats] = useState<Chats[] | null>(null);
+  async function fetchChats() {
+    const token = Cookies.get("token");
+    try {
+      const { data } = await axios.get(`${chat_service}/api/v1/chat/all`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setChats(data.chats);
+    } catch (error) {
+      console.log(error);
     }
   }
-
+  const [users, setUsers] = useState<User[] | null>(null);
+  async function fetchUsers() {
+    const token = Cookies.get("token");
+    try {
+      const { data } = await axios.get(`${user_service}/api/v1/user/all`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   useEffect(() => {
     fetchUser();
     fetchChats();
+    fetchUsers();
   }, []);
-
   return (
-    <AppContext.Provider value={{ user, setUser, isAuth, setIsAuth, loading }}>
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+        isAuth,
+        setIsAuth,
+        loading,
+        logoutUser,
+        fetchChats,
+        fetchUsers,
+        chats,
+        users,
+        setChats,
+      }}
+    >
       {children}
       <Toaster />
     </AppContext.Provider>
