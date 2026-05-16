6d6db5cf9ea5d3cd968077dd2c88a55e2a968a22
rdsymlntsa <rdsymlntsa@gmail.com>
2026-05-16 09:53:33 +0530

    feat: Add chat sidebar to chat layout

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,20 +1,75 @@
 "use client";
 
+import ChatSidebar from "@/components/ChatSidebar";
 import Loading from "@/components/Loading";
-import { useAppData } from "@/context/AppContext";
+import { useAppData, User } from "@/context/AppContext";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+export interface Message {
+  _id: string;
+  chatId: string;
+  sender: string;
+  text?: string;
+  image?: {
+    url: string;
+    publicId: string;
+  };
+  messageType: "text" | "image";
+  seen: boolean;
+  seenAt?: string;
+  createdAt: string;
+}
 
 const ChatApp = () => {
-  const { loading, isAuth } = useAppData();
+  const {
+    loading,
+    isAuth,
+    logoutUser,
+    chats,
+    user: loggedInUser,
+    users,
+    fetchChats,
+    setChats,
+  } = useAppData();
+
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  const [message, setMessage] = useState("");
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [messages, setMessages] = useState<Message[] | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [showAllUsers, setShowAllUsers] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [typingTimeOut, setTypingTimeOut] = useState<NodeJS.Timeout | null>(
+    null,
+  );
+
   const router = useRouter();
   useEffect(() => {
     if (!isAuth && !loading) {
       router.push("/login");
     }
   }, [isAuth, loading, router]);
+
+  const handleLogout = () => logoutUser();
+
   if (loading) return <Loading />;
-  return <div>ChatApp</div>;
+  return (
+    <div className="min-h-screen flex bg-green-900 text-white relative overflow-hidden">
+      <ChatSidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        showAllUsers={showAllUsers}
+        setShowAllUsers={setShowAllUsers}
+        users={users}
+        loggedInUser={loggedInUser}
+        chats={chats}
+        selectedUser={selectedUser}
+        setSelectedUser={setSelectedUser}
+        handleLogout={handleLogout}
+      />
+    </div>
+  );
 };
 
 export default ChatApp;
